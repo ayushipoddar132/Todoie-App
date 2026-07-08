4d78ac98934053adb6b167ab353cdfedab80d58c
ayushipoddar132 <ayushipoddar132@gmail.com>
2026-07-08 22:58:53 +0530

    Add Header component and render Todo heading

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from "react";
 
+import Header from "./components/Header";
+
 const App = () => {
-  return <p>Todoie App</p>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 };
 
 export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,8 @@
-import React from 'react';
+import React from "react";
 
-import Header from './components/Header';
 
 const Header = () => {
     return <h1> Todo </h1>
 };
 
-export default App;
+export default Header;
