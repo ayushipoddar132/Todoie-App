2c5e2d02f40944ce35b8409721d6cce2d31d556f
ayushipoddar132 <ayushipoddar132@gmail.com>
2026-07-08 23:27:36 +0530

    Add Button component and render it in App

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 
 import Header from "./components/Header";
 import TodoItem from "./components/TodoItem";
+import Button from "./components/Button";
 const App = () => {
   return (
     <div>
       <Header />
       <TodoItem />
+      <Button />
     </div>
   );
 };
