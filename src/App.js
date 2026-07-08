4b21ca06b798fd08fda9830357967e18a3cbe1b9
ayushipoddar132 <ayushipoddar132@gmail.com>
2026-07-08 23:06:41 +0530

    Add TodoItem component and render it in App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 
 import Header from "./components/Header";
-
+import TodoItem from "./components/TodoItem";
 const App = () => {
   return (
     <div>
       <Header />
+      <TodoItem />
     </div>
   );
 };
